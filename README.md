412adfb38b97384dfaf91df389832ba117c4dcd4
M Atheer <55885230+masreplay@users.noreply.github.com>
2023-09-18 15:20:30 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# messenger_app
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/messenger_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.messenger_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
